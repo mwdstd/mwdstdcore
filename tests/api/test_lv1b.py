a140249002bd92c7b0306ea39649ddfbfac2820b
DrManac <drploskikh@gmail.com>
2022-10-28 16:20:34 +0300

    Basic autoprocessing tests

--- tests/api/test_lv1b.py
+++ tests/api/test_lv1b.py
@@ -8,5 +8,22 @@ def client():
     return app.test_client()
 
 def test_main(client: FlaskClient):
-    response = client.post("/v1/lv1b", json={})
-    assert response.status_code == 400
+    response = client.post("/v1/lv1b", json={
+        'runs': [
+            {
+                'surveys': [
+                    {'md': 0, 'gx': 1, 'gy': 1, 'gz': 1, 'bx': 1, 'by': 2, 'bz': 2,}
+                ],
+                'reference': [
+                    {'g': 0, 'b': 0, 'dip': 0}
+                ],
+                'dni_rigid': True,
+            }
+        ],
+        'geomag': 'hdgm',
+        'head_ref': {'g': 0, 'b': 0, 'dip': 0}
+    })
+
+    assert response.status_code == 200
+    assert response.is_json
+    assert 'corrections' in response.json
--- tests/api/test_lv2b.py
+++ tests/api/test_lv2b.py
@@ -7,6 +7,31 @@ from mwdstdcore.server import app
 def client():
     return app.test_client()
 
-def test_lv2b(client: FlaskClient):
-    response = client.post('/v1/lv2b')
-    assert response
+def test_main(client: FlaskClient):
+    response = client.post("/v1/lv2b", json={
+        'runs': [
+            {
+                'surveys': [
+                    {'md': 0, 'gx': 1, 'gy': 1, 'gz': 1, 'bx': 1, 'by': 2, 'bz': 2,}
+                ],
+                'reference': [
+                    {'g': 0, 'b': 0, 'dip': 0}
+                ],
+                'dni_rigid': True,
+                'bha': {
+                    'structure': [
+                        {'type': 'bit', 'od': 1, 'id': 0, 'weight': 0, 'length': 0, 'material': 'steel'}
+                    ],
+                    'blades': [],
+                    'dni_to_bit': 0,
+                },
+                'mud_weight': 0,
+            }
+        ],
+        'geomag': 'hdgm',
+        'head_ref': {'g': 0, 'b': 0, 'dip': 0},
+    })
+
+    assert response.status_code == 200
+    assert response.is_json
+    assert 'corrections' in response.json
