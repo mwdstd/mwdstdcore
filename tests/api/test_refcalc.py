2c0fb88b8abc12688449178fc26298fc910eb1b5
DrManac <drploskikh@gmail.com>
2022-10-28 12:57:22 +0300

    Refcalc testing

--- tests/api/test_refcalc.py
+++ tests/api/test_refcalc.py
@@ -7,6 +7,36 @@ from mwdstdcore.server import app
 def client():
     return app.test_client()
 
-def test_refcalc(client: FlaskClient):
-    response = client.post('/v1/refcalc')
-    assert response
+def test_point(client: FlaskClient):
+    response = client.post('/v1/refcalc', json={
+        'latitude': 0,
+        'longitude': 0,
+        'altitude': 0,
+        'date': '2020-09-01T00:00:00.000Z',
+        'gmag_mod': 'WMM2020',
+        'crustal_field': False,
+    })
+    assert response.status_code == 200
+    assert response.is_json
+    assert response.json['err_model']['name'] == 'WMM'
+    assert 'base_point' in response.json
+    assert 'points' not in response.json
+
+def test_traj(client: FlaskClient):
+    response = client.post('/v1/refcalc', json={
+        'latitude': 0,
+        'longitude': 0,
+        'altitude': 0,
+        'date': '2020-09-01T00:00:00.000Z',
+        'gmag_mod': 'WMM2020',
+        'crustal_field': False,
+        'plan': [
+            {'md': 0, 'inc': 0, 'az': 0},
+            {'md': 0, 'inc': 0, 'az': 0},
+        ]
+    })
+    assert response.status_code == 200
+    assert response.is_json
+    assert response.json['err_model']['name'] == 'WMM'
+    assert 'base_point' in response.json
+    assert 'points' in response.json
